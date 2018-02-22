055449f51dc4604b2701404b70c230365f2519d2
arsal <kharsala18@gmail.com>
2018-02-22 13:27:18 -0500

    created the welcome page index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+  <link href="app.css" rel="stylesheet" type="text/css" >
+  <title>Welcome</title>
+</head>
+
+<body>
+
+    <header>
+      <nav id="#ac-globalnav">
+
+          <a href="registration.php">Sign-Up</a>
+          <a href="login.php">Sign-In</a>
+
+      </nav>
+  </header>
+    <h1 class="names"> Assignment 2 </h1>
+
+    <img src="" >
+
+    <div class="footer"
+    <footer>
+      <h2>Developers</h2>
+        <ul>
+          <li class="names">Arsalan Khan</li>
+          <li class="names">Hasib Aziz</li>
+        </ul>
+          <h2 id="cpright">Copyrights Khan and Aziz...2018</h2>
+
+    </footer>
+  </div>
+</body>
+
+
+
+</html>
