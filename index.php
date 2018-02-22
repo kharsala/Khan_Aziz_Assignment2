cfef231054748b1c24f9a9eac1b8066dc112f451
arsal <kharsala18@gmail.com>
2018-02-22 11:49:57 -0500

    Updated the index and registration page

--- index.php
+++ index.php
@@ -14,22 +14,3 @@
       </body>
 
 </html>
-
-//Validates password & confirm passwords.
-    if(!empty($_POST["password"]) && ($_POST["password"] == $_POST["cpassword"])) {
-        $password = test_input($_POST["password"]);
-        $cpassword = test_input($_POST["cpassword"]);
-        if (strlen($_POST["password"]) <= '8') {
-            $passwordErr = "Your Password Must Contain At Least 8 Characters!";
-        }
-        elseif(!preg_match("#[0-9]+#",$password)) {
-            $passwordErr = "Your Password Must Contain At Least 1 Number!";
-        }
-        elseif(!preg_match("#[A-Z]+#",$password)) {
-            $passwordErr = "Your Password Must Contain At Least 1 Capital Letter!";
-        }
-        elseif(!preg_match("#[a-z]+#",$password)) {
-            $passwordErr = "Your Password Must Contain At Least 1 Lowercase Letter!";
-        } else {
-            $cpasswordErr = "Please Check You've Entered Or Confirmed Your Password!";
-        }
--- registration.php
+++ registration.php
@@ -29,8 +29,8 @@
           //variables that hold regex for validation
            $reg_name = "/^[a-zA-Z ]*$/";
            $reg_email="/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/";
-           $reg_code ="/^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/";
-           $reg_phone ="/\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/";
+          // $reg_code ="/^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/";
+           //$reg_phone ="/\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/";
            //variables hold the array of words to be removed from address and bio fields
            //$sql_keywords = array("select", "insert", "update", "delete", "#", "--");
           // $sql_replace = array("choose","place","improve","cut", "", "");
@@ -45,28 +45,14 @@
             if(preg_match($reg_email, $_POST['email']))
               $email = strip_tags(trim( $_POST['email']));
 
-            if(isset($_POST['address'])){
+            
 
-              if(strlen($_POST['address']) <= 256)
-                  $address =str_ireplace($sql_keywords, $sql_replace, htmlentities(strip_tags(trim($_POST['address']), ENT_NOQUOTES)));
-
-            }
-            if(preg_match($reg_code, $_POST['code']))
-            $code = strip_tags(trim($_POST['code']));
-
-            if(preg_match($reg_phone, $_POST['phone']))
-                  $phone =strip_tags(trim($_POST['phone']));
-
-            if(isset($_POST['bio'])){
-                   $bio =str_ireplace($sql_keywords, $sql_replace,  htmlentities(strip_tags(trim($_POST['bio']), ENT_NOQUOTES)));
-
-
-            }
+          }
             //variable that hold database information
             $dbhost = 'localhost' ;
             $username = 'root' ;
             $password = '';
-            $database = 'userdatabase' ;
+            $database = 'users' ;
 
             //creating connection to the database
             $mysqli = new mysqli("$dbhost", "$username", "$password", "$database");
@@ -78,7 +64,7 @@
             }
 
             //sql query to insert form data into database table (userTable)
-            $sql = "insert into usertable (UserName, EmailAddress, Address, PostalCode, PhoneNumber, UserBIO)
+            $sql = "insert into user (UserName, EmailAddress, Password)
             Values ('$user', '$email', '$address', '$code', '$phone', '$bio')";
 
             if (mysqli_query($mysqli, $sql)) {
@@ -104,6 +90,8 @@
           <input type="text" name="email" pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="Invalid Email!!!" required/><br>
           Password:<br>
           <input type="password" name="password" required/><br>
+          retype-Password:
+              <input type="password" name="rpassword" required/><br>
           <input type="submit" name="submit" value="register" />
 
      </form>
