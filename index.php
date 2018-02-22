388ace877e9600fee8744da1b1d5e1d2df7015f4
arsal <kharsala18@gmail.com>
2018-02-22 11:15:46 -0500

    created this page as a welcome page to the blog.

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+      <head> <title>Welcome</title></head>
+      <body>
+        <div>
+            <header></header>
+        <div>
+        <div>
+          
+
+        </div>
+        <div>
+            <footer></footer>
+        </div>
+      </body>
+
+</html>
