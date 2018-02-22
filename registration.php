47258064c3d1facba9824cb5635dbdf339fab6ed
arsal <kharsala18@gmail.com>
2018-02-22 11:29:01 -0500

    created this file for user regitration. Curruntly implementing the form and database.

---
+++ registration.php
@@ -0,0 +1,118 @@
+<html>
+
+    <head>
+
+      <title>Register</title>
+      <link href="app.css" rel="stylesheet" type="text/css" >
+      <meta character="UTF-8">
+
+    </head>
+    <body>
+
+
+      <div class="navigation">
+
+                  <header>
+
+                    <nav id="#ac-globalnav">
+
+                        <a href="registration.php">New-User</a>
+                        <a href="index.php">List-Users</a>
+                    </nav>
+
+                  </header>
+      </div>
+
+      <h1>New-User</h1>
+    <div>
+     <?php
+          //variables that hold regex for validation
+           $reg_name = "/^[a-zA-Z ]*$/";
+           $reg_email="/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/";
+           $reg_code ="/^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/";
+           $reg_phone ="/\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/";
+           //variables hold the array of words to be removed from address and bio fields
+           //$sql_keywords = array("select", "insert", "update", "delete", "#", "--");
+          // $sql_replace = array("choose","place","improve","cut", "", "");
+
+           //when landing on the page cheks if form has been submited
+          if(isset($_POST['submit']))
+          {
+            //data entered in the form on newUser.php page
+            if((string)preg_match($reg_name, $_POST['name']))
+              $user = strip_tags(trim($_POST['name'] ));
+
+            if(preg_match($reg_email, $_POST['email']))
+              $email = strip_tags(trim( $_POST['email']));
+
+            if(isset($_POST['address'])){
+
+              if(strlen($_POST['address']) <= 256)
+                  $address =str_ireplace($sql_keywords, $sql_replace, htmlentities(strip_tags(trim($_POST['address']), ENT_NOQUOTES)));
+
+            }
+            if(preg_match($reg_code, $_POST['code']))
+            $code = strip_tags(trim($_POST['code']));
+
+            if(preg_match($reg_phone, $_POST['phone']))
+                  $phone =strip_tags(trim($_POST['phone']));
+
+            if(isset($_POST['bio'])){
+                   $bio =str_ireplace($sql_keywords, $sql_replace,  htmlentities(strip_tags(trim($_POST['bio']), ENT_NOQUOTES)));
+
+
+            }
+            //variable that hold database information
+            $dbhost = 'localhost' ;
+            $username = 'root' ;
+            $password = '';
+            $database = 'userdatabase' ;
+
+            //creating connection to the database
+            $mysqli = new mysqli("$dbhost", "$username", "$password", "$database");
+            if($mysqli->connect_error){
+               die("Connection Failed" . $conn->connect_error);
+            }
+            else{
+              echo "Connection Successful" . "<br>" ;
+            }
+
+            //sql query to insert form data into database table (userTable)
+            $sql = "insert into usertable (UserName, EmailAddress, Address, PostalCode, PhoneNumber, UserBIO)
+            Values ('$user', '$email', '$address', '$code', '$phone', '$bio')";
+
+            if (mysqli_query($mysqli, $sql)) {
+              echo "New record created successfully";
+            }
+            else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($mysqli);
+            }
+          }
+
+
+
+      ?>
+    </div>
+
+
+    <div class="main-body-Form">
+     <form id="userForm" action="newUser.php" method="post">
+
+          UserName:<br>
+          <input type="text" name="name" pattern="[a-zA-Z]{1,15}" title ="UserName should only contain letters e.g KHAN or khan" required/><br>
+          E-mail:<br>
+          <input type="text" name="email" pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="Invalid Email!!!" required/><br>
+          Password:<br>
+          <input type="password" name="password" required/><br>
+          <input type="submit" name="submit" value="register" />
+
+     </form>
+     <div>
+
+    <div class="footer">
+     <footer id="cpright" >copyrights Arsalan Khan And Hasib Aziz ..... 2018</footer>
+   </div>
+    </body>
+
+
+</html>
