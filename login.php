a172ad2802513600e947014267f8b3cd03728abd
arsal <kharsala18@gmail.com>
2018-02-24 13:36:31 -0500

    updated the login page added user session and authentecated the usr

--- login.php
+++ login.php
@@ -1,111 +1,57 @@
 <?php
-
+session_start();
  require_once 'ConnectorDb.php';
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = "";
 
-// Prepare a select statement
-$sql = "SELECT username, password FROM users WHERE UserName = ? AND Password = ?";
-//$stmt = $mysqli -> prepare ("SELECT UserName, Password FROM users where UserName = '$user' AND Password = '$password' ");
-if($stmt = mysqli_prepare($mysqli, $sql)){
-    // Bind variables to the prepared statement as parameters
-  //  mysqli_stmt_bind_param($stmt, "ss", $param_user, $param_password);
-  $stmt = $mysqli -> prepare ( $sql  );
-
-  $stmt ->bind_param("ss", $param_user, $param_password );
-
-// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $username = strip_tags($_POST['username']);
+  $password = strip_tags($_POST['password']);
 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = 'Please enter username.';
-    } else{
-        $username = trim($_POST["username"]);
-    }
+  $username =  stripslashes($username);
+  $password =  stripslashes($password);
 
-    // Check if password is empty
-    if(empty(trim($_POST['password']))){
-        $password_err = 'Please enter your password.';
-    } else{
-        $password = trim($_POST['password']);
-    }
-    //variable that hold database information
+  $username = mysqli_real_escape_string($mysqli, $username);
+  $password= mysqli_real_escape_string($mysqli, $password);
 
-    if(empty($user_err) && empty($password_err)){
+  //$password= md5($password);
 
-            // Set parameters
-            $param_user = $username;
-            $param_password = $password;
+  $dbQuery = "SELECT * FROM users WHERE UserName = '$username' LIMIT 1";
+  $result = mysqli_query($mysqli, $dbQuery);
+  $row = mysqli_fetch_array($result);
 
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
+  $Id = $row['Id'];
+  $db_password = $row['Password'];
 
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $user, $password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify( $password)){
-                            /* Password is correct, so start a new session and
-                            save the username to the session */
-                            session_start();
-                            $_SESSION['username'] = $user;
-                            header("location: index.php");
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = 'Invalid Password Entered';
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = 'No User Account Exits';
-                }
-            } else{
-                echo "Error: Please Try Again";
-            }
-        }
+  if($password == $db_password){
+    $_SESSION['username'] = $username;
+    $_SESSION['id'] = $Id;
+    header("Location: userPage.php");
 
-        // Close statement
-        $stmt->execute();
-        $stmt->close();
-    }
+  }else{
+    echo "Invalid Username or Password";
+  }
 
-    // Close connection
-    mysqli_close($mysqli);
+
+
+
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
+<html >
 <head>
-    <meta charset="UTF-8">
     <title>Login</title>
     <link href="app.css" rel="stylesheet" type="text/css" >
 
 </head>
 <body>
     <div div class="main-body-Form">
-        <h2>Login</h2>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                Username:<br>
-                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                Password:<br>
-                <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <p>Don't have an account? <a id = "login" href="registration.php">Sign-Up</a></p>
-        </form>
+        <h1>Login</h1>
+      <form action="login.php" method="post">
+        UserName:<br>
+        <input type="text" name="username" required/><br>
+        <input type="password" name="password" required/><br>
+        <input type="submit" name="submit" value="Login"/>
+        <input type="reset" value="Clear"/>
+      </form>
+        <p>Don't have an account? <a id = "login" href="registration.php">Sign-Up</a></p>
     </div>
 
     <div class="footer">
