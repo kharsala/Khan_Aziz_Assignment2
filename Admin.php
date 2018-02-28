4bcbeb8911c2fa34ec271652579daf4a6355a305
h-aziz <hasib.aziz23@gmail.com>
2018-02-28 04:10:25 -0500

    admin page almost completed

--- Admin.php
+++ Admin.php
@@ -1,36 +1,103 @@
-<html>
-<head>
-  <link href="app.css" rel="stylesheet" type="text/css" >
-  <title>Admin-Page</title>
-</head>
+<?php include('AdminOptions.php');
 
+ //Start session
+session_start();
+
+include_once('ConnectorDb.php');
+
+  if(isset($_GET['edit'])) {
+    $id = $_GET['edit'];
+    $edit_state = true;
+    $rec = mysqli_query($database, "SELECT * FROM users WHERE id=$id");
+    $record = mysqli_fetch_array($rec);
+    $username = $record['username'];
+    $email = $record['email'];
+    $password = $record['password'];
+    $id = $recprd['id'];
+
+  }
+
+
+?>
+
+<html >
+<head>
+    <title>Administration Page</title>
+    <link href="app.css" rel="stylesheet" type="text/css" >
+
+</head>
 <body>
 
-    <header>
-      <nav id="#ac-globalnav">
+  <?php if (isset($_SESSION['message'])): ?>
+      <div class="message">
+        <?php
+          echo $_SESSION['message'];
+          unset($_SESSION['message']);
+         ?>
+      </div>
+  <?php endif ?>
+  <table>
+        <h1>Welcome To Admin Page <?php echo $_SESSION['username'] ?> </h1>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>E-Mail</th>
+            <th>Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php while($row = mysqli_fetch_array($results)){ ?>
+            <tr>
+              <td><?php echo $row['username'] ?> </td>
+              <td><?php echo $row['email'] ?> </td>
+              <td><?php echo $row['password'] ?> </td>
+              <td>
+                <a class="edit_btn" href="Admin.php?edit=<?php echo $row['id']; ?>">Edit</a>
+              </td>
+              <td>
+                <a class="delete_btn href="AdminOptions.php?delete=<?php echo $row['id']; ?>">Delete</a>
+              </td>
+              <td>
+                <a href="#">Approve</a>
+              </td>
+            </tr>
+        <?php  } ?>
 
-          <a href="userDetails.php">View-Users</a>
-          <a href="">Logout</a>
-          <a href ="ProfileMgmt.php"> My-Profile</a>
 
-      </nav>
+        </tbody>
+      </table>
 
-  </header>
-    <h1 class="names"> Admin </h1>
+      <form method="post" action="AdminOptions.php">
+        <input type="hidden" name="id" value="<?php echo $id; ?>">
+        <div class="input-group">
+          <label>Username</label>
+          <input type="text" name="username" value="<?php echo $username; ?>">
+        </div>
+        <div class="input-group">
+          <label>EmailAddress</label>
+          <input type="text" name="email" value="<?php echo $email; ?>">
+        </div>
+        <div class="input-group">
+          <label>Password</label>
+          <input type="text" name="password" value="<?php echo $password; ?>">
+        </div>
+        <div class="input-group">
+          <?php if ($edit_state == false): ?>
+            <button type="submit" name="create" class="btn">Create User</button>
+          <?php else: ?>
+            <button type="submit" name="edit" class="btn">Edit</button>
+            <?php endif; ?>
 
-    <img src="" >
+        </div>
+      </form>
 
-    <div class="footer"
-    <footer>
-      <h2>Developers</h2>
-        <ul>
-          <li class="names">Arsalan Khan</li>
-          <li class="names">Hasib Aziz</li>
-        </ul>
-          <h2 id="cpright">Copyrights Khan and Aziz...2018</h2>
 
-    </footer>
-  </div>
+
+    <div class="footer">
+     <footer id="cpright" >Copyrights of Arsalan Khan And Hasib Aziz ..... 2018</footer>
+   </div>
 </body>
 
 </html>
+
+//https://www.youtube.com/watch?v=mjVuBlwXASo
