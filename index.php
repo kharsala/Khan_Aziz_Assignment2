234d5050421bbe097b4ad8d6e2466d782ccc12a6
h-aziz <hasib.aziz23@gmail.com>
2018-03-05 20:09:27 -0500

    added file path for log.txt

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
   session_start();
  include_once('ConnectorDb.php');
+
 ?>
+
+<?php
+ini_set('display_errors',1);
+ini_set('log_errors',1);
+ini_set('error_log', dirname(__FILE__) . '/log.txt');
+error_reporting(E_ALL);
+
+require 'logtest.com';
+
+ ?>
 
 
 <html>
